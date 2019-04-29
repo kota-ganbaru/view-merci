9dcc2342009968ab5bd177fb7e1fd38dfbbb22a0
kota-koyama <kota-koyama@dac.co.jp>
2019-04-29 17:41:57 +0900

    木村が元々作っていたやつをコピって、そこにviewを追加したソース

---
+++ src/main/java/com/mercipcsp/MerciPcspApplication.java
@@ -0,0 +1,13 @@
+package com.mercipcsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerciPcspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerciPcspApplication.class, args);
+	}
+
+}
